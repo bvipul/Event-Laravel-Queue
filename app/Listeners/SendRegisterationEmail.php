707a8035ed0474e07df56dd03b4ad40e4a9d14d7
Vipul Basapati <basapativipulkumar@gmail.com>
2018-04-02 10:50:53 +0530

    Initial commit with Project files

---
+++ app/Listeners/SendRegisterationEmail.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserCreated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class SendRegisterationEmail implements ShouldQueue
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  UserCreated  $event
+     * @return void
+     */
+    public function handle(UserCreated $event)
+    {
+        $user = $event->user;
+        
+        //Send Email Code Here
+    }
+}
